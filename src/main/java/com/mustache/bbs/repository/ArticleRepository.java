b5ed7b287e64694ee409be5f002b818a3f268342
momineee <p.rainmakerm@gmail.com>
2022-11-07 16:07:57 +0900

    ArticleRepository - 주석 추가

--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // Dao의 추상화 버전
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    // <관리대상Entity, 대표값의 타입(Article의 대표값 id의 타입은 long)>
 }
