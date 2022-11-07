4305798d0eccacfe025637ccdf845db94fb3d90d
momineee <p.rainmakerm@gmail.com>
2022-11-07 14:30:22 +0900

    ArticleController 작성

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,15 +1,24 @@
 package com.mustache.bbs.controller;
 
+import com.mustache.bbs.domain.dto.ArticleDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/articles") // 여기서 articles 해주면 밑에서 안써도 됨
 @Slf4j
 public class ArticleController {
-        @GetMapping(value = "/articles/new")
-        public String newArticleForm() {
-            return "articles/new";
-        }
+    @GetMapping(value = "/new")
+    public String newArticleForm() {
+        return "articles/new";
     }
+    @PostMapping(value = "/posts")
+    public String createArticle(ArticleDto form) {
+        log.info(form.toString());
+        return "";
+    }
+}
 
